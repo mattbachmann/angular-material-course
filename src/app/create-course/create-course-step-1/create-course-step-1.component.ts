d5cd83eb991105d69aa972d90c916c1bcd0e7f71
mattbachmann <matth.bachmann@gmail.com>
2024-01-05 15:02:19 +0100

    mat-textarea

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -5,10 +5,11 @@ import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
+  templateUrl: "create-course-step-1.component.html",
   styleUrls: ["create-course-step-1.component.scss"]
 })
 export class CreateCourseStep1Component {
+  private readonly SAMPLE_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 'Vivamus quis tempor massa. Donec euismod elit vitae enim varius, ' + 'et ultrices lacus tincidunt. Sed eu nisi sit amet magna lobortis ' + 'consequat. Nullam aliquam urna sed aliquet lobortis. ' + 'Suspendisse potenti. Nullam nec diam et dolor lobortis ' + 'condimentum. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ' + 'Nulla facilisi. Nulla facilisi. Nulla facilisi. ';
 
   form = this.fb.group({
     title: ['', [
@@ -20,10 +21,10 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [this.SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
 
-  getDateClass : MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+  getDateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     const dayOfWeek = cellDate.getDay();
     return (dayOfWeek === 0 || dayOfWeek === 6) ? 'highlight-date' : '';
   };
